2fa3693c02086ff2265ed1a20f04a6d1106f7d81
_Emmanuel <adewaleemmanuel93@gmail.com>
2022-11-18 13:27:35 +0100

    livewire SPA -post model

---
+++ app/Http/Livewire/Posts.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Post;
+
+class Posts extends Component
+{
+    public $posts, $title, $description, $post_id;
+    public $isOpen = 0;
+
+    public function render()
+    {
+        $this->posts = Post::all();
+        return view('livewire.posts');
+    }
+
+    public function create()
+    {
+        $this->resetInputFields ();
+        $this->openModal ();
+    }
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+
+    }
+
+    public function closeModal()
+    {
+        $this->isOpen = false;
+
+    }
+
+    private function resetInputFields() {
+        $this->title = ' ';
+        $this->description = ' ';
+        $this->post_id = ' ';
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Post::updateOrCreate(['id' => $this->post_id], [
+            'title' => $this->title,
+            'description' => $this->description
+        ]);
+
+        session()->flash('message',
+            $this->post_id ? 'Post Updated Successfully.' : 'Post Created Successfully.');
+
+            $this->closeModal();
+            $this->resetInputFields();
+
+    }
+
+    public function edit($id) {
+        $post = Post::findOrFail($id);
+        $this->post_id = $id;
+        $this->title = $post->title;
+        $this->description = $post->description;
+
+        $this->openModal();
+    }
+}
