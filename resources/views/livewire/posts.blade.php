f23f01fbce6304c2eea2f890c0b32e3a8578ddb2
_Emmanuel <adewaleemmanuel93@gmail.com>
2022-11-18 13:49:43 +0100

    livewire post view

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,3 +1,35 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    @if($updateMode)
+        @include('livewire.update')
+    @else
+        @include('livewire.create')
+    @endif
+
+    <table class="table table bordered mt-5">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Title</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $value)
+            <tr>
+                <td>{{$value->id}}</td>
+                <td>{{$value->title}}</td>
+                <td>{{$value->description}}</td>
+                <td>
+                    <button wire:click="edit({{ $value->id }})" class="btn btn-primary btn-sm">
+                        Edit
+                    </button>
+                    <button wire:click="delete({{ $value->id }})" class="btn btn-danger btn-sm">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+
+    </table>
 </div>
