64ce884300da281fe5eb93634334be029c94905b
_Emmanuel <adewaleemmanuel93@gmail.com>
2022-11-18 13:35:08 +0100

    Live wire post component updated

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -68,4 +68,11 @@ class Posts extends Component
 
         $this->openModal();
     }
+
+    public function delete ($id) {
+        Post::find($id)->delete();
+        session()->flash('message', 'Post Deleted Successfully');
+    }
+
+
 }
